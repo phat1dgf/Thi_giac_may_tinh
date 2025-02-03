dd588fbd8d817d58de99a02c72c4e007eed7ad9d
QiJi- <146053594+phat1dgf@users.noreply.github.com>
2025-02-03 13:35:05 +0700

    ddadasd

--- detect_train.py
+++ detect_train.py
@@ -4,7 +4,7 @@ import torch
 if __name__ == '__main__':
     print(torch.cuda.is_available())
     torch.cuda.empty_cache()
-    MODEL_PATH = "weights/yolo11n.pt"
+    MODEL_PATH = "weights/yolo11s.yaml"
 
     model = YOLO(MODEL_PATH)
 
